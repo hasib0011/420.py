f587b85c19499baafc6736c72a9701bf971f91aa
hasib0011 <88262011+hasib0011@users.noreply.github.com>
2021-08-25 00:59:43 +0600

    420.py
    pkg install git 
    git clone https://github.com/hasib0011/420.py
    ls
    cd 420.py
    ls 
    bash 420.py

---
+++ 420.py
@@ -0,0 +1,82 @@
+import os
+import sys
+import time
+os.system("pkg install figlet")
+os.system("clear")
+os.system("figlet HASIB420")
+print("Enter your name")
+name = input() 
+
+time.sleep(2)
+
+print("░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\n")
+print("☞ Auther     : Hasib420 ")
+print("☞ WhatsApp   : +8801864186795")
+print("☞ Facebook I'd   :https://www.facebook.com/profile.php?id=100044107367152")
+print("░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\n")
+
+print("Type viktim user ")
+hasib = input() 
+
+
+time.sleep(3)
+print("░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\n")
+print("----------------Ddos attack started!----------------")
+print("░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\n")
+time.sleep(4)
+print("░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░")
+print(":::::::::::::::::IP adress tracked!:::::::::::::::::")
+print("░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\n")
+time.sleep(5)
+print("░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░")
+print("~~~~~~~~~~~~~~Bypassing all security~~~~~~~~~~~~~~~~")
+print("░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\n")
+
+time.sleep(6)
+print("viktim,",hasib,"and pas 123456")
+
+print("IP adress: 127.0.0.5")
+
+
+time.sleep(6)
+print("Congratulations,",name,"! You have successfully hacked:")
+
+time.sleep(8)
+os.system("figlet HASIB420")
+
+print("Type IP adress")
+name = input() 
+
+time.sleep(7)
+
+print("░░░░░░░░░░░▄▄▀▀▀▀▀▀▀▀▄▄")
+print("░░░░░░░░▄▀▀░░░░░░░░░░░░▀▄▄")
+print("░░░░░░▄▀░░░░░░░░░░░░░░░░░░▀▄")
+print("░░░░░▌░░░░░░░░░░░░░▀▄░░░░░░░▀▀▄")
+print("░░░░▌░░░░░░░░░░░░░░░░▀▌░░░░░░░░▌")
+print("░░░▐░░░░░░░░░░░░▒░░░░░▌░░░░░░░░▐")
+print("░░░▌▐░░░░▐░░░░▐▒▒░░░░░▌░░░░░░░░░▌")
+print("░░▐░▌░░░░▌░░▐░▌▒▒▒░░░▐░░░░░▒░▌▐░▐")
+print("░░▐░▌▒░░░▌▄▄▀▀▌▌▒▒░▒░▐▀▌▀▌▄▒░▐▒▌░▌")
+print("░░░▌▌░▒░░▐▀▄▌▌▐▐▒▒▒▒▐▐▐▒▐▒▌▌░▐▒▌▄▐")
+print("░▄▀▄▐▒▒▒░▌▌▄▀▄▐░▌▌▒▐░▌▄▀▄░▐▒░▐▒▌░▀▄")
+print("▀▄▀▒▒▌▒▒▄▀░▌█▐░░▐▐▀░░░▌█▐░▀▄▐▒▌▌░░░▀")
+print("░▀▀▄▄▐▒▀▄▀░▀▄▀░░░░░░░░▀▄▀▄▀▒▌░▐")
+print("░░░░▀▐▀▄▒▀▄░░░░░░░░▐░░░░░░▀▌▐")
+print("░░░░░░▌▒▌▐▒▀░░░░░░░░░░░░░░▐▒▐")
+print("░░░░░░▐░▐▒▌░░░░▄▄▀▀▀▀▄░░░░▌▒▐")
+print("░░░░░░░▌▐▒▐▄░░░▐▒▒▒▒▒▌░░▄▀▒░▐")
+print("░░░░░░▐░░▌▐▐▀▄░░▀▄▄▄▀░▄▀▐▒░░▐")
+print("░░░░░░▌▌░▌▐░▌▒▀▄▄░░░░▄▌▐░▌▒░▐")
+print("░░░░░▐▒▐░▐▐░▌▒▒▒▒▀▀▄▀▌▐░░▌▒░▌")
+print("░░░░░▌▒▒▌▐▒▌▒▒▒▒▒▒▒▒▐▀▄▌░▐▒▒")
+
+
+
+time.sleep(8)
+os.system("figlet HASIB420")
+time.sleep(9)
+os.system("exit")
+
+
+
